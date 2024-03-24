2f1ca52de3769863eb5065fd2ed57ba81c33e405
Abdiselam13 <salmaanade404@gmail.com>
2024-03-24 10:18:16 +0300

    exercise

---
+++ script.js
@@ -0,0 +1,16 @@
+// window.prompt("enter your password")
+//  let passwod = 1122
+// if(passwod ===  1122){
+//      console.log("your password is correct")
+// }
+// else if(passwod !== 1122){
+//     console.log("your password is inc")
+// }
+// window.prompt("your password is incorect")
+
+// window.prompt(' to know your balance enter 1 ')
+
+// let balance = 1
+// if(balance === 1 ){
+//    window.prompt("your balance is $210")
+// }
